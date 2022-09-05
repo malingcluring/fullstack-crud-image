2c978d61201847e6214bede331b54038562310a4
malingcluring <maling.maling.maling.maling@gmail.com>
2022-09-05 16:43:02 +0700

    update

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import FileUpload from "express-fileupload";
+import cors from "cors";
+import path from "path";
+import PrductRoute from './routes/ProductRoute.js';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(FileUpload());
+app.use(express.static("public"));
+app.use(PrductRoute);
+
+app.listen(5000, ()=> console.log('Server up and running!...'));
